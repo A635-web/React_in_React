7b7d1d7fb7f795ea94f61924fd087f98d3366277
A635-web <adi2004dixit@gmail.com>
2024-06-17 20:47:11 +0530

    injecting bundlers

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
+// const heading=React.createElement("div",{id:"parent"},
+//     React.createElement("div",{id:"child"},
+//         [React.createElement("h1",{},"I am an h1 tag"),
+//         React.createElement("h2",{},"I am in h2 tag"), 
+//         ]
+//     )
+    
+   
+    
+// );
+// console.log(heading);
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom";
 const heading=React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child"},
         [React.createElement("h1",{},"I am an h1 tag"),
